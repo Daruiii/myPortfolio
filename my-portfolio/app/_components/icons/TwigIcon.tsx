6833ac0b570c8e2ef6e62053c61a783216e243ab
David MEGUIRA <davidmgr93@gmail.com>
2026-06-25 19:23:25 +0200

    fix: TwigIcon inline style to prevent Tailwind Preflight height:auto warning

--- my-portfolio/app/_components/icons/TwigIcon.tsx
+++ my-portfolio/app/_components/icons/TwigIcon.tsx
@@ -3,18 +3,17 @@ import Image from "next/image";
 
 type ImageProps = Omit<ComponentPropsWithoutRef<typeof Image>, "src" | "alt">;
 
-interface ESGIIconProps extends ImageProps {
+interface TwigIconProps extends ImageProps {
   size?: number;
 }
 
-export const TwigIcon = ({ size = 40, ...props }: ESGIIconProps) => {
-  return (
-    <Image
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8IX7Y_6pHJR-CSKGif7wRISvEScSzjbD3vQ&s"
-      alt="TwigIcon"
-      width={size}
-      height={size}
-      {...props}
-    />
-  );
-};
+export const TwigIcon = ({ size = 40, style, ...props }: TwigIconProps) => (
+  <Image
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8IX7Y_6pHJR-CSKGif7wRISvEScSzjbD3vQ&s"
+    alt="Twig"
+    width={size}
+    height={size}
+    style={{ width: size, height: size, ...style }}
+    {...props}
+  />
+);
